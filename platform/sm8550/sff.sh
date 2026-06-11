251c96f3c1d429dd4f62380982d575e79c23e93b
gyabii <gyabii@proton.me>
2026-06-11 13:32:52 +0300

    sm8550: commonize sff.sh
    Change-Id: I26e036a0b59c985199ccbcb56d1ce6e84ce82ac6
    Signed-off-by: gyabii <gyabii@proton.me>

--- platform/sm8550/sff.sh
+++ platform/sm8550/sff.sh
@@ -1,7 +1,7 @@
 # Copyright (c) 2026 Salvo Giangreco
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-# SEC Floating Feature configuration file for Galaxy S23+ (dm2q)
+# SEC Floating Feature configuration file for Snapdragon 8 Gen 2 devices (sm8550)
 
 # Enable Wallpaper AOD feature
 SEC_FLOATING_FEATURE_LCD_CONFIG_AOD_BRIGHTNESS_ANIMATION=1
